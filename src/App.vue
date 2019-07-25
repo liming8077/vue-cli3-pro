59000a91b6396a63d410efb80ca8dbf21089b502
liming <liming@dfwsgroup.com>
2019-07-25 10:57:05 +0800

    搭建

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <navbar />
+    <content-view></content-view>
+    <request-loding></request-loding>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import ContentView from '@/components/ContentView.vue'
+import RequestLoding from '@/components/RequestLoading.vue'
+
+export default {
+  components: {
+    Navbar, ContentView, RequestLoding
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app{
+  // position: fixed;
+  // left: 0;
+  // right: 0;
+  // top:0;
+  // bottom:0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import ElementUI from 'element-ui'
+import Cookies from 'js-cookie'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss'
+import '@/icons'
+
+import common from '@/utils/common' // 全局方法
+import filters from '@/utils/filters' // 全局过滤器
+import '@/utils/permission'
+
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+// 注册过滤器
+for (let key in filters) {
+  Vue.filter(key, filters[key])
+}
+
+Vue.use(common)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
